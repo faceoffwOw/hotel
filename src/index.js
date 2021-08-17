880d6a9368c322d6cd7a63f98ac624e492789ff3
faceoffwOw <get_right101@mail.ru>
2021-08-17 12:33:04 +0300

    guests block

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ const input_start = document.getElementById("start_one");
 const input_end = document.getElementById("end_one")
 let i_start = document.getElementById("start_expand");
 let i_end = document.getElementById("end_expand");
+let span_check = document.getElementById("info");
+let div = document.getElementById("container__info");
+let i_guests = document.getElementById("expand_guests");
+let count_div = document.getElementById("count_div");
 
 addEventListener("click", function() {
   if(input_start === document.activeElement){
@@ -20,3 +24,17 @@ addEventListener("click", function() {
     i_end.classList.replace("fa-angle-up", "fa-angle-down");
   }
 });
+
+window.addEventListener('click', function (e) {
+  if(span_check != e.target){
+    div.classList.replace("expand", "not-expand");
+    i_guests.classList.replace("fa-angle-up", "fa-angle-down");
+    count_div.style.display = "none";
+  }
+});
+
+function spanClick(){
+  div.classList.replace("not-expand", "expand");
+  i_guests.classList.replace("fa-angle-down", "fa-angle-up");
+  count_div.style.display = "block"; 
+}
