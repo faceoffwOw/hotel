e6125da77ce7de205ac896df9e197706d5e22035
faceoffwOw <get_right101@mail.ru>
2021-08-27 16:05:19 +0300

    checkboxes

--- src/js/search.js
+++ src/js/search.js
@@ -1,5 +1,8 @@
 let date_residence = document.getElementById('date_residence');
 let angle_residence = document.getElementById('angle_residence');
+let checkbox = document.querySelectorAll('input[type=checkbox]');
+let divCheck = document.querySelectorAll('.checkbox');
+
 
 addEventListener("click", function() {
   if(date_residence === document.activeElement){
@@ -9,3 +12,19 @@ addEventListener("click", function() {
     angle_residence.classList.replace("fa-angle-up", "fa-angle-down");
   }
 });
+
+
+
+for(let i = 0; i < divCheck.length; i++){
+
+  divCheck[i].addEventListener("click", function(){
+    divCheck[i].classList.toggle("active");
+
+    if(divCheck[i].classList.contains("active")){
+      checkbox[i].checked = true;
+    }
+    else{
+      checkbox[i].checked = false;
+    }
+  })
+}
